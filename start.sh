20f427054869abccdf6c95a66f893e2bcf6096a6
leandrosardi <leandro.sardi@expandedventure.com>
2023-11-19 07:37:11 -0300

    start and stop script

--- start.sh
+++ start.sh
@@ -1,11 +1,18 @@
 # kill all micro.data processed except postgress
-ps ax | grep "code/my.saas" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
+ps ax | grep "code/app" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9 >/dev/null 2>&1;
 # start webserver
 export RUBYLIB=~/code
 cd ~/code/my.saas
 ruby $HOME/code/code/app/app.rb port=3000 config=./config.rb > /dev/null 2>&1 &
-#ruby $HOME/code/code/app/p/dispatcher.rb > /dev/null 2>&1 &
-#ruby $HOME/code/code/app/p/worker.rb > /dev/null 2>&1 &
-#ruby $HOME/code/code/app/extensions/i2p/p/ipn.rb > /dev/null 2>&1 &
+#ruby $HOME/code/code/app/p/actions.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/deletion.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/delivery.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/export.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/leads.upload.import.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/leads.upload.ingest.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/planner.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/p/timeline.rb > /dev/null 2>&1 &
+#ruby $HOME/code/code/app/p/receive.rb > /dev/null 2>&1 &
+ruby $HOME/code/code/app/extensions/i2p/p/ipn.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
 #ruby $HOME/code/code/app/extensions/i2p/p/baddebt.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
 #ruby $HOME/code/code/app/extensions/i2p/p/expire.rb > /dev/null 2>&1 & # ==> Test this process before put it on production.
--- stop.sh
+++ stop.sh
@@ -1,6 +1,14 @@
 # kill all micro.data processed except postgress
 ps ax | grep "puma" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "sync.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "ipn.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "baddebt.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "expire.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+#ps ax | grep "actions.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "deletion.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "delivery.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "export.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "leads.upload.import.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "leads.upload.ingest.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "planner.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "timeline.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+#ps ax | grep "receive.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "ipn.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "baddebt.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
+ps ax | grep "expire.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
