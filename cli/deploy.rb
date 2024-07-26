3d0ab6fc682bfea439d2541eaa695c097137f8c1
leandrosardi <leandro.sardi@expandedventure.com>
2024-07-26 18:23:00 -0300

    improve deployer

--- cli/deploy.rb
+++ cli/deploy.rb
@@ -64,163 +64,167 @@ parser = BlackStack::SimpleCommandLineParser.new(
   }]
 )
 
-# grab the name of the routine
-routine_name = parser.value('routine') == '-' ? nil : parser.value('routine')
+begin
+  # grab the name of the routine
+  routine_name = parser.value('routine') == '-' ? nil : parser.value('routine')
 
-# run database updates
-l.logs 'Deploy database migrations?... '
-if !parser.value('db')
-  l.logf 'no'.red 
-else
-  l.logf 'yes'.green 
+  # run database updates
+  l.logs 'Deploy database migrations?... '
+  if !parser.value('db')
+    l.logf 'no'.red 
+  else
+    l.logf 'yes'.green 
 
-  # TODO: make a better way to decide which nodes are master nodes
-  l.logs 'Loading master nodes (nodes that serve as database servers)... ' 
-  masters = BlackStack::Deployer.nodes.select { |n| 
-    n.name=~/#{parser.value('nodes')}/ && 
-    !n.parameters[:db_type].nil? && 
-    n.parameters[:dev].to_s != 'true' 
-  }
-  l.logf 'done'.green + " (#{masters.size.to_s.blue} nodes)"
+    # TODO: make a better way to decide which nodes are master nodes
+    l.logs 'Loading master nodes (nodes that serve as database servers)... ' 
+    masters = BlackStack::Deployer.nodes.select { |n| 
+      n.name=~/#{parser.value('nodes')}/ && 
+      !n.parameters[:db_type].nil? && 
+      n.parameters[:dev].to_s != 'true' 
+    }
+    l.logf 'done'.green + " (#{masters.size.to_s.blue} nodes)"
 
-  masters.each { |node|
-    l.logs "Node #{node.name.blue}... "
+    masters.each { |node|
+      l.logs "Node #{node.name.blue}... "
 
-    if node.parameters[:db_type] != :pg && node.parameters[:db_type] != :crdb
-      l.logf 'skipped'.yellow + " (only :pg and :crdb are supported)"
-    end
+      if node.parameters[:db_type] != :pg && node.parameters[:db_type] != :crdb
+        l.logf 'skipped'.yellow + " (only :pg and :crdb are supported)"
+      end
 
-    if node.parameters[:db_type] == :pg
-      BlackStack::PostgreSQL::set_db_params({ 
-        :db_url => node.net_remote_ip, 
-        :db_port => node.parameters[:db_port],
-        :db_name => node.parameters[:db_name],
-        :db_user => node.parameters[:db_user],
-        :db_password => node.parameters[:db_password],
-      })
-    end
+      if node.parameters[:db_type] == :pg
+        BlackStack::PostgreSQL::set_db_params({ 
+          :db_url => node.net_remote_ip, 
+          :db_port => node.parameters[:db_port],
+          :db_name => node.parameters[:db_name],
+          :db_user => node.parameters[:db_user],
+          :db_password => node.parameters[:db_password],
+        })
+      end
 
-    if node.parameters[:db_type] == :crdb
-      BlackStack::CockroachDB::set_db_params({
-        :db_url => node.net_remote_ip,
-        :db_cluster => node.parameters[:db_cluster],
-        :db_sslmode => node.parameters[:db_sslmode],
-        :db_port => node.parameters[:db_port],
-        :db_name => node.parameters[:db_name],
-        :db_user => node.parameters[:db_user],
-        :db_password => node.parameters[:db_password],
-      })
-    end
+      if node.parameters[:db_type] == :crdb
+        BlackStack::CockroachDB::set_db_params({
+          :db_url => node.net_remote_ip,
+          :db_cluster => node.parameters[:db_cluster],
+          :db_sslmode => node.parameters[:db_sslmode],
+          :db_port => node.parameters[:db_port],
+          :db_name => node.parameters[:db_name],
+          :db_user => node.parameters[:db_user],
+          :db_password => node.parameters[:db_password],
+        })
+      end
 
-    l.logs 'Connecting the database... '
-    BlackStack::Deployer::DB.connect(
-      BlackStack.connection_string # use the connection parameters setting in ./config.rb
-    )
-    l.logf 'done'.green
+      l.logs 'Connecting the database... '
+      BlackStack::Deployer::DB.connect(
+        BlackStack.connection_string # use the connection parameters setting in ./config.rb
+      )
+      l.logf 'done'.green
 
-    l.logs 'Loading checkpoint... '
-    BlackStack::Deployer::DB::load_checkpoint("./my-ruby-deployer.lock")
-    label = BlackStack::Deployer::DB::checkpoint.to_s.blue
-    label = 'no checkpoint'.yellow if label.to_s.empty?
-    l.logf "done".green + " (#{label})"
-
-    l.logs 'Running database updates... '
-    BlackStack::Deployer::DB::set_folder ('../sql')
-    BlackStack::Deployer::DB::deploy(true, "./my-ruby-deployer.lock", l)
-    l.logf 'done'.green
-
-    # run the .sql scripts of each extension
-    BlackStack::Extensions.extensions.each { |e|
-      l.logs "Loading checkpoint for #{e.name.downcase.blue}... "
-      BlackStack::Deployer::DB::load_checkpoint("./my-ruby-deployer.#{e.name.downcase}.lock")
+      l.logs 'Loading checkpoint... '
+      BlackStack::Deployer::DB::load_checkpoint("./my-ruby-deployer.lock")
       label = BlackStack::Deployer::DB::checkpoint.to_s.blue
       label = 'no checkpoint'.yellow if label.to_s.empty?
       l.logf "done".green + " (#{label})"
 
-      l.logs "Running database updates for #{e.name.downcase.blue}... "
-      BlackStack::Deployer::DB::set_folder ("../extensions/#{e.name.downcase}/sql")
-      BlackStack::Deployer::DB::deploy(true, "./my-ruby-deployer.#{e.name.downcase}.lock", l)
+      l.logs 'Running database updates... '
+      BlackStack::Deployer::DB::set_folder ('../sql')
+      BlackStack::Deployer::DB::deploy(true, "./my-ruby-deployer.lock", l)
       l.logf 'done'.green
-    }
 
-    # disconnecting from the database
-    l.logs 'Disconnecting the database... '
-    #BlackStack::Deployer::DB.disconnect
-    #l.logf 'done'.green
-    l.logf "TODO: develop disconnect method!"
+      # run the .sql scripts of each extension
+      BlackStack::Extensions.extensions.each { |e|
+        l.logs "Loading checkpoint for #{e.name.downcase.blue}... "
+        BlackStack::Deployer::DB::load_checkpoint("./my-ruby-deployer.#{e.name.downcase}.lock")
+        label = BlackStack::Deployer::DB::checkpoint.to_s.blue
+        label = 'no checkpoint'.yellow if label.to_s.empty?
+        l.logf "done".green + " (#{label})"
 
-    l.logf 'done'.green
+        l.logs "Running database updates for #{e.name.downcase.blue}... "
+        BlackStack::Deployer::DB::set_folder ("../extensions/#{e.name.downcase}/sql")
+        BlackStack::Deployer::DB::deploy(true, "./my-ruby-deployer.#{e.name.downcase}.lock", l)
+        l.logf 'done'.green
+      }
 
-  } # masters.each
-end # if parser.value('db')
+      # disconnecting from the database
+      l.logs 'Disconnecting the database... '
+      #BlackStack::Deployer::DB.disconnect
+      #l.logf 'done'.green
+      l.logf "TODO: develop disconnect method!"
 
-# run deployment routine to each node 
-l.logs 'Install/Update source code... '
-if !parser.value('code')
-  l.logf 'no'.red
-else
-  # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
-  BlackStack::Deployer.nodes.select { |n| 
-    n.name=~/#{parser.value('nodes')}/ && 
-    n.parameters[:dev].to_s != 'true' 
-  }.each { |n|
-    node_name = n.name  
-
-    l.logs "Node #{node_name.blue}... "
-    BlackStack::Deployer.run_routine(node_name, routine_name || n.deployment_routine, l)
-    l.logf 'done'.green
-  } # BlackStack::Deployer.nodes
-
-  l.logf 'done'.green
-end # if parser.value('code')
-
-# update the source code of extensions
-l.logs 'Install/Update extensions... '
-if !parser.value('ext')
-  l.logf 'no'.red
-else
-  # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
-  BlackStack::Deployer.nodes.select { |n| 
-    n.name=~/#{parser.value('nodes')}/ &&
-    n.parameters[:dev].to_s != 'true'
-  }.each { |n|
-    node_name = n.name  
-
-    l.logs "Node #{node_name.blue}... "
-    BlackStack::Extensions.extensions.each { |e|
-      l.logs "Updating code for #{e.name.downcase.to_s.blue}... "
-      BlackStack::Deployer.run_routine(node_name, 'extension', l, {
-        # name of the git repository
-        :extension_name => e.name.downcase, 
-        # name of git user beloning the repository of this extension
-        :repo_url => e.repo_url, 
-        # branch to download from
-        :repo_branch => e.repo_branch, 
-      })
       l.logf 'done'.green
-    }
-    l.logf 'done'.green
-  } # BlackStack::Deployer.nodes
 
-  l.logf 'done'.green
-end # if parser.value('ext')
+    } # masters.each
+  end # if parser.value('db')
 
-# update the source code of extensions
-l.logs 'Restart processes... '
-if !parser.value('restart')
-  l.logf 'no'.red
-else
-  # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
-  BlackStack::Deployer.nodes.select { |n| 
-    n.name=~/#{parser.value('nodes')}/ &&
-    n.parameters[:dev].to_s != 'true'
-  }.each { |n|
-    l.logs "Node #{n.name.blue}... "
-    n.connect
-    n.stop(OUTPUT_FILE)
-    n.start(OUTPUT_FILE)
-    n.disconnect
+  # run deployment routine to each node 
+  l.logs 'Install/Update source code... '
+  if !parser.value('code')
+    l.logf 'no'.red
+  else
+    # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
+    BlackStack::Deployer.nodes.select { |n| 
+      n.name=~/#{parser.value('nodes')}/ && 
+      n.parameters[:dev].to_s != 'true' 
+    }.each { |n|
+      node_name = n.name  
+
+      l.logs "Node #{node_name.blue}... "
+      BlackStack::Deployer.run_routine(node_name, routine_name || n.deployment_routine, l)
+      l.logf 'done'.green
+    } # BlackStack::Deployer.nodes
+
     l.logf 'done'.green
-  } # BlackStack::Deployer.nodes
-  l.logf 'done'.green
-end # if parser.value('ext')
+  end # if parser.value('code')
+
+  # update the source code of extensions
+  l.logs 'Install/Update extensions... '
+  if !parser.value('ext')
+    l.logf 'no'.red
+  else
+    # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
+    BlackStack::Deployer.nodes.select { |n| 
+      n.name=~/#{parser.value('nodes')}/ &&
+      n.parameters[:dev].to_s != 'true'
+    }.each { |n|
+      node_name = n.name  
+
+      l.logs "Node #{node_name.blue}... "
+      BlackStack::Extensions.extensions.each { |e|
+        l.logs "Updating code for #{e.name.downcase.to_s.blue}... "
+        BlackStack::Deployer.run_routine(node_name, 'extension', l, {
+          # name of the git repository
+          :extension_name => e.name.downcase, 
+          # name of git user beloning the repository of this extension
+          :repo_url => e.repo_url, 
+          # branch to download from
+          :repo_branch => e.repo_branch, 
+        })
+        l.logf 'done'.green
+      }
+      l.logf 'done'.green
+    } # BlackStack::Deployer.nodes
+
+    l.logf 'done'.green
+  end # if parser.value('ext')
+
+  # update the source code of extensions
+  l.logs 'Restart processes... '
+  if !parser.value('restart')
+    l.logf 'no'.red
+  else
+    # TODO: each node should have its routine, so deployer should run the assigned routine to each node, no matter of it is web or pampa.
+    BlackStack::Deployer.nodes.select { |n| 
+      n.name=~/#{parser.value('nodes')}/ &&
+      n.parameters[:dev].to_s != 'true'
+    }.each { |n|
+      l.logs "Node #{n.name.blue}... "
+      n.connect
+      n.stop(OUTPUT_FILE)
+      n.start(OUTPUT_FILE)
+      n.disconnect
+      l.logf 'done'.green
+    } # BlackStack::Deployer.nodes
+    l.logf 'done'.green
+  end # if parser.value('ext')
+rescue => e
+  l.error(e)
+end
