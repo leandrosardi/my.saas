50857a42e6b73d91b1cf6fad1d919e949d1210d9
leandrosardi <leandro.sardi@expandedventure.com>
2024-09-01 12:06:00 -0300

    https://github.com/MassProspecting/docs/issues/207

---
+++ models/stub/deployment/deployment.rb
@@ -0,0 +1,14 @@
+module BlackStack
+  module Deployment
+    @@nodes = []
+
+    def self.add_node(h)
+    end # add_node
+
+    def self.nodes
+      @@nodes
+    end # nodes
+
+  end # Deployment
+end # BlackStack
+  
