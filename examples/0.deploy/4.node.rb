22418ffcc91b03580edfa97d6c89159a707230e1
leandrosardi <leandro.sardi@expandedventure.com>
2024-09-07 20:07:59 -0300

    https://github.com/MassProspecting/docs/issues/208

---
+++ examples/0.deploy/4.node.rb
@@ -0,0 +1,78 @@
+# load gem and connect database
+#require 'mysaas'
+#require 'lib/stubs'
+
+require_relative '/home/leandro/code/blackstack-nodes/lib/blackstack-nodes.rb'
+require_relative '/home/leandro/code/my.saas/models/stub/deployment/deployment.rb'
+
+require 'config'
+
+#require 'config'
+#DB = BlackStack.db_connect
+#require 'lib/skeletons'
+
+l = BlackStack::LocalLogger.new('deploy-examples.log')
+
+begin
+    #l.logs('Validate SAASLIB environment... ')
+    #BlackStack::Deployment.validate_saaslib_env
+    #l.done
+
+    l.logs('Add node demo-node... ')
+    BlackStack::Deployment.add_node({
+        # Unique name to identify a host (a.k.a.: node).
+        # It is equal to the hostname of the node.
+        #
+        # Mandatory.
+        #
+        # Allowed values: string with a valid hostname.
+        #
+        :name => 'demo-node', 
+        
+        # Public Internet IP address of the node.
+        # If `nil`, that means that no instance of a cloud server has been created yet for this node.
+        #
+        # Optional. Default: `nil`.
+        #
+        # Allowed values: string with a valid IP address.
+        # 
+        :net_remote_ip => '85.190.240.229',
+        
+        # Reference to another node where is hosted the database to work with.
+        #
+        # Mandatory.
+        #
+        # Allowed values: string with a valid PostgreSQL database name,
+        # 
+        :db_host => 'demo-node',    
+    }.merge(
+        T[:prod], 
+        T[:web]
+    ))
+    l.done
+
+    l.logs('Get node demo-node... ')
+    n = BlackStack::Deployment.get_node('demo-node')
+    l.done
+
+    l.logs('Connect to node demo-node... ')
+    n.connect
+    l.done
+    # => n.ssh
+    
+    l.logs 'Valid command: `hostname`... ' 
+    l.logf n.exec('hostname').blue
+    # => 'dev1'
+
+    l.logs 'Invalid command: `rm`... ' 
+    l.logf n.exec('rm').blue
+    # => 'dev1'
+
+    l.logs 'Disconnect from node demo-node... '
+    n.disconnect
+    l.done
+    # => nil
+
+rescue => e
+    l.error(e)
+end
--- models/stub/deployment/deployment.rb
+++ models/stub/deployment/deployment.rb
@@ -2,78 +2,6 @@ module BlackStack
   module Deployment
     @@nodes = []
 
-    # Validates that the environment variable SAASLIB is defined.
-    #
-    # This function checks if the environment variable SAASLIB is set.
-    # If SAASLIB is not defined, it raises a RuntimeError with an appropriate message.
-    # 
-    # @raise [RuntimeError] If the SAASLIB environment variable is not defined.
-    #
-    # @example
-    #   validate_saaslib_env
-    #   # If SAASLIB is not defined, this will raise an exception with the message:
-    #   # "Environment variable SAASLIB is not defined."
-    #
-    def self.validate_saaslib_env
-      unless ENV['SAASLIB']
-        raise "Environment variable SAASLIB is not defined."
-      end
-    end
-
-    # Reference:
-    # https://github.com/MassProspecting/docs/issues/254
-    #
-    # Executes a given bash command, redirecting its standard output and error output to specified files.
-    # 
-    # The output and error files are cleaned (truncated) before the command is executed.
-    # If the command produces any error output, an exception is raised with the error message.
-    # If the command executes successfully, the content of the standard output is returned.
-    # 
-    # By default, the output and error files are located in the directory specified by the $SAASLIB environment variable.
-    # 
-    # @param command [String] The bash command to be executed.
-    # @param output_file [String] (default: '$SAASLIB/output.txt') The file where standard output will be redirected.
-    # @param error_file [String] (default: '$SAASLIB/error.txt') The file where standard error will be redirected.
-    # 
-    # @return [String] The content of the standard output if the command executes successfully.
-    # 
-    # @raise [RuntimeError] If the command produces any error output, the exception will contain the error message.
-    # 
-    # @example Basic usage
-    #   begin
-    #     output = execute_command('ls /some_directory')
-    #     puts output
-    #   rescue => e
-    #     puts e.message
-    #   end
-    #
-    def self.execute_command(command, output_file: "#{ENV['SAASLIB']}/bash-command-stdout-buffer", error_file: "#{ENV['SAASLIB']}/bash-command-stderr-buffer")
-      # Clean the output and error files before execution
-      File.open(output_file, 'w') {} # Truncate the output file
-      File.open(error_file, 'w') {}  # Truncate the error file
-
-      # Execute the command, redirecting stdout to the output file and stderr to the error file
-      system("#{command} > #{output_file} 2> #{error_file}")
-
-      # Check if the error file has any content
-      if File.size?(error_file)
-        # Read the error message
-        error_message = File.read(error_file)
-        # Raise an exception with the error message
-        raise "Command failed with the following error:\n#{error_message}"
-      end
-
-      # If no error, read and return the content of the output file
-      File.read(output_file)
-    end
-  
-    def self.nodes
-      @@nodes
-    end # nodes
-
-
-
-
     def self.add_node(h)
       err = []
 
@@ -190,10 +118,60 @@ module BlackStack
       raise ArgumentError, "The following errors were found in My.SaaS node descriptor: \n#{err.map { |s| " - #{s}" }.join("\n")}" unless err.empty?
 
       # Create and add the node
-      node = BlackStack::Infrastructure::Node.new(h)
-      @@nodes << node
-    end
+      @@nodes << h
+    end # def self.add_node(h)
 
+    # 
+    def self.get_node(node_name)
+      @@nodes.find { |n| n[:name] == node_name }
+    end # def self.get_node(node_name)
+
+    # 
+    def self.install(
+      node_name,
+      logger: nil,
+      bash_script_url: 'https://raw.githubusercontent.com/leandrosardi/environment/main/sh/install.ubuntu.20_04.sh'
+    )
+      l = logger || BlackStack::DummyLogger.new(nil)
+
+      l.logs "Getting node #{node_name.blue}... "
+      n = get_node(node_name)
+      raise ArgumentError, "Node not found: #{node_name}" if n.nil?
+      n = n.clone # clone the hash descriptor, because I will modify it below.
+      l.done
+
+      # download the file from the URL
+      l.logs "Downloading bash script from #{bash_script_url.blue}... "
+      bash_script = Net::HTTP.get(URI(bash_script_url))
+      l.done(details: "#{bash_script.length} bytes downloaded")
+
+      # switch user to root and create the node object
+      l.logs "Creating node object... "
+      new_ssh_username = n[:ssh_username]
+      new_hostname = n[:name]
+      n[:ssh_username] = 'root'
+      n[:ssh_password] = n[:ssh_root_password]
+      node = BlackStack::Infrastructure::Node.new(n)
+      l.done
+
+      l.logs('Connect to node demo-node... ')
+      node.connect
+      l.done
+      # => n.ssh
+
+      # execute the script line by line
+      bash_script.each_line { |line|
+        l.logs "#{line.blue}... "
+        line.gsub!('$1', new_hostname)
+        line.gsub!('$2', new_ssh_username)
+        l.logf node.exec(line)
+      }
+
+      l.logs 'Disconnect from node demo-node... '
+      node.disconnect
+      l.done
+
+    end # def self.install(node_name, logger: nil)
 
 
   end # Deployment
