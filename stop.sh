2d3466410fcb4a3bcd75608431a5ebd75c35e876
leandrosardi <leandro.sardi@expandedventure.com>
2024-09-21 17:12:15 -0300

    removing bash scripts in favor of new deployment library

--- stop.sh
+++
@@ -1,6 +0,0 @@
-# kill all micro.data processed except postgress
-ps ax | grep "puma" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "sync.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "ipn.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "baddebt.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
-#ps ax | grep "expire.rb" | grep -v postgres | grep -v grep | cut -b1-7 | xargs -t kill -9
