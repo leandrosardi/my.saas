4f2852e966ebcdbcac45e498eccbe5f7e41cf601
leandrosardi <leandro.sardi@expandedventure.com>
2024-11-21 11:33:10 -0300

    removing backups feature - start implementing secret repositories and blackops

--- lib/stubs.rb
+++ lib/stubs.rb
@@ -1,9 +1,6 @@
-require 'my-ruby-deployer'
-
 require 'models/stub/extensions/leftbaricon'
 require 'models/stub/extensions/dependency'
 require 'models/stub/extensions/extension'
 require 'models/stub/extensions/settingscreen'
 require 'models/stub/funnels/funnel'
-require 'models/stub/backups/backup'
 require 'models/stub/transactional/transactional'
