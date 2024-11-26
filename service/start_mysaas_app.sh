50e6ccf7beb8e7709e36fb8507687a44a905e4ae
leandrosardi <leandro.sardi@expandedventure.com>
2024-11-26 15:29:15 -0300

    https://github.com/leandrosardi/blackops/issues/43

---
+++ service/start_mysaas_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load RVM into a shell session
+source /etc/profile.d/rvm.sh
+
+# Use the default Ruby version
+rvm --default use 3.1.2
+
+# Set the RUBYLIB environment variable
+export RUBYLIB=/home/!!ssh_username/code1/!!code_folder
+
+# Change to the application directory
+cd /home/!!ssh_username/code1/!!code_folder
+
+# Execute the Ruby application
+exec ruby app.rb
